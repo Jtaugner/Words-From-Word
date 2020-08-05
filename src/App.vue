f29bdf710681b8df81b501acbc40880f066560c7
Jtaugner <rak123rak123@mail.ru>
2020-08-05 14:52:56 +0300

    Translate English letter to Russian during input word

--- src/App.vue
+++ src/App.vue
@@ -1129,6 +1129,9 @@
   let newLevel = new NewAudioContext('new-level');
   let clickSound = new NewAudioContext('click');
 
+  const lettersMap = {
+      'q' : 'й', 'w' : 'ц', 'e' : 'у', 'r' : 'к', 't' : 'е', 'y' : 'н', 'u' : 'г', 'i' : 'ш', 'o' : 'щ', 'p' : 'з', '[' : 'х', ']' : 'ъ', 'a' : 'ф', 's' : 'ы', 'd' : 'в', 'f' : 'а', 'g' : 'п', 'h' : 'р', 'j' : 'о', 'k' : 'л', 'l' : 'д', ';' : 'ж', '\'' : 'э', 'z' : 'я', 'x' : 'ч', 'c' : 'с', 'v' : 'м', 'b' : 'и', 'n' : 'т', 'm' : 'ь', ',' : 'б', '.' : 'ю','Q' : 'Й', 'W' : 'Ц', 'E' : 'У', 'R' : 'К', 'T' : 'Е', 'Y' : 'Н', 'U' : 'Г', 'I' : 'Ш', 'O' : 'Щ', 'P' : 'З', '[' : 'Х', ']' : 'Ъ', 'A' : 'Ф', 'S' : 'Ы', 'D' : 'В', 'F' : 'А', 'G' : 'П', 'H' : 'Р', 'J' : 'О', 'K' : 'Л', 'L' : 'Д', ';' : 'Ж', '\'' : 'Э', 'Z' : '?', 'X' : 'ч', 'C' : 'С', 'V' : 'М', 'B' : 'И', 'N' : 'Т', 'M' : 'Ь', ',' : 'Б', '.' : 'Ю'
+  };
 
   function params(data) {
     try{
@@ -1334,6 +1337,7 @@
           this.sendWord();
         } else{
           letter = letter.toLowerCase();
+          if(lettersMap[letter]) letter = lettersMap[letter];
           for(let i = 0; i < this.letters.length; i++){
             if(letter === this.letters[i] && !this.selectedLetters.includes(i)){
               this.selectedLetters.push(i);
